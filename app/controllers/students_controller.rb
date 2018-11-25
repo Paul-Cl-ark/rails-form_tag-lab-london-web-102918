0c0b44e3904fd33a3b6ec630ef7068a71d1be933
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-25 12:30:25 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,31 @@
 class StudentsController < ApplicationController
-  
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def new
+    # @student = Student.new
+  end
+
+  def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to students_path
+  end
+
+  private
+
+  def set_student
     @student = Student.find(params[:id])
   end
+
+  # def student_params
+  #   params.require(:student).permit(:first_name, :last_name)
+  # end
 
 end
